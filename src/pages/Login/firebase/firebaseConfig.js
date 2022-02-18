af3a0514ea8329deb758c0fc39a12d6d29785d2b
mdshohidulislam1486 <Shohidul Islam>
2022-02-18 23:37:02 +0600

    u

--- src/pages/Login/firebase/firebaseConfig.js
+++ src/pages/Login/firebase/firebaseConfig.js
@@ -1,5 +1,5 @@
 
-/* const firebaseConfig = {
+const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_PROJECT_ID,
@@ -7,5 +7,5 @@
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID,
 };
-export default firebaseConfig; */
+export default firebaseConfig;
 
